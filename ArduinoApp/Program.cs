3aade8e687c5f4b1543473b68811ac9506731793
Abdulaziz <40052862+Abdukhalil-zoda@users.noreply.github.com>
2021-07-15 22:05:30 +0500

    publish

---
+++ ArduinoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ArduinoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort my = new SerialPort();
+            my.BaudRate = 9600;
+            my.PortName = "COM3";
+            my.Open();
+
+            while (true)
+            {
+                var s = Console.ReadLine();
+                my.WriteLine(s);
+            }
+
+        }
+    }
+}
